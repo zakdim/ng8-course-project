72b1d5cc1898ae5441cdf37e67ae897ccc0a7720
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-10-16 22:38:06 -0400

    Implement recipe service, s10, lecture 116

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -11,16 +12,12 @@ export class RecipeListComponent implements OnInit {
   @Output('recipeWasSelected') recipeWasSelected =
     new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test',
-      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test',
-      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg') 
-  ];
+  recipes: Recipe[];
  
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,17 @@
+import { Recipe } from './recipe.model';
 
 export class RecipeService {
 
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test',
+      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'),
+    new Recipe('Another Test Recipe', 'This is simply a test',
+      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg') 
+  ];
+
   constructor() { }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
