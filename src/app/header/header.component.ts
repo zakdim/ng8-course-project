0a5cc20c20c4b6b2371cd702d67c62a926014c1f
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-11-10 16:07:00 -0500

    Mark active routes
    section 12, lecture 157

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,12 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  // @Output('featureSelected') featureSelected = new EventEmitter<string>();
-  
   collapsed = true;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  // onSelect(feature: string) {
-  //   this.featureSelected.emit(feature);
-  // }
 }
