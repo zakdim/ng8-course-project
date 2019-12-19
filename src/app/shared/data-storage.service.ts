87d2185a44d20e62fd38828517f905f94e8bbea7
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-12-18 21:54:34 -0500

    Fetching recipes
    section 19, lecture 281

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({
     providedIn: 'root'
@@ -18,6 +19,14 @@ export class DataStorageService {
                 recipes
             ).subscribe(response => {
                 console.log(response);
+            });
+    }
+
+    fetchRecipes() {
+        this.http.get<Recipe[]>(
+                'https://ng-course-recipe-book-481ca.firebaseio.com/recipes.json'
+            ).subscribe(recipes => {
+                this.recipeService.setRecipes(recipes);
             });
     }
 }
