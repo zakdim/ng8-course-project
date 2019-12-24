2df8871a2d233906162c3797e447cd63b7ce1719
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-12-24 18:16:45 -0500

    Transforming response data
    section 19, lecture 282

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 
@@ -23,10 +25,14 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http.get<Recipe[]>(
-                'https://ng-course-recipe-book-481ca.firebaseio.com/recipes.json'
-            ).subscribe(recipes => {
-                this.recipeService.setRecipes(recipes);
+        this.http.get<Recipe[]>('https://ng-course-recipe-book-481ca.firebaseio.com/recipes.json')
+        .pipe(map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
             });
+        }))
+        .subscribe(recipes => {
+            this.recipeService.setRecipes(recipes);
+        });
     }
 }
