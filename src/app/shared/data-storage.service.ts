f5547b953ea2b9334eda9d918d592d1eb29daf70
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-12-25 13:44:56 -0500

    Section 19: Course project - Http - final
    section 19, lecture 284

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -25,14 +25,20 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http.get<Recipe[]>('https://ng-course-recipe-book-481ca.firebaseio.com/recipes.json')
-        .pipe(map(recipes => {
-            return recipes.map(recipe => {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-            });
-        }))
-        .subscribe(recipes => {
-            this.recipeService.setRecipes(recipes);
-        });
+        return this.http
+            .get<Recipe[]>('https://ng-course-recipe-book-481ca.firebaseio.com/recipes.json')
+            .pipe(
+                map(recipes => {
+                    return recipes.map(recipe => {
+                        return {
+                          ...recipe, 
+                         ingredients: recipe.ingredients ? recipe.ingredients : []
+                     };
+                 });
+                }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes);
+                })
+            );
     }
 }
