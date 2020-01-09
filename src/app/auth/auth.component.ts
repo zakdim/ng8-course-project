ca27955753faca854dfd5aadb3381202720bbe71
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-01-08 21:41:37 -0500

    Sending the signup request
    section 20, lecture 293

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -10,7 +11,7 @@ export class AuthComponent implements OnInit {
 
   isLoginMode = true;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -21,6 +22,25 @@ export class AuthComponent implements OnInit {
 
   onSubmit(form: NgForm) {
     console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+      // ...
+    } else {
+      this.authService.signup(email, password).subscribe(
+        resData => {
+          console.log(resData);
+        }, 
+        error => {
+          console.log(error);
+        }
+      );
+    }
+
     form.reset();
   }
 }
