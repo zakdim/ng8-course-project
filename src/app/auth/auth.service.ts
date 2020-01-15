25cf835e5a88f9ba0d5fa41922c16eed441b0a89
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-01-15 09:38:33 -0500

    20-303 Adding auto-login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -68,6 +68,28 @@ export class AuthService {
       );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email, 
+      userData.id, 
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -84,6 +106,8 @@ export class AuthService {
       expirationDate
     );
     this.user.next(user);
+    // persist to local storage
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
